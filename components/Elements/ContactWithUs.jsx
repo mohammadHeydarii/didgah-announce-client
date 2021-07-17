954783aacb284ff871669fe10e2d0f6a782b0139
Mohammad2000dev <mohammad.heydari7977@gmail.com>
2021-07-17 21:24:09 +0430

    A button for contact with us

---
+++ components/Elements/ContactWithUs.jsx
@@ -0,0 +1,11 @@
+import Styles from "../../styles/ContactWithUs.module.css";
+
+const ContactWithUs = () => {
+  return (
+    <div className={Styles.contactButton}>
+      <button className="btn btn-primary rounded-pill">تماس با ما</button>
+    </div>
+  );
+};
+
+export default ContactWithUs;
