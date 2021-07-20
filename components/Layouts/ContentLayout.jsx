52f9f1e2e9e3f723eade6ea6ed8bfa06c51b74a9
sajadadineh <sajadadineh79@gmail.com>
2021-07-20 21:01:53 +0430

    modified

--- components/Layouts/ContentLayout.jsx
+++ components/Layouts/ContentLayout.jsx
@@ -3,24 +3,32 @@ import { Grid } from "@material-ui/core";
 import Image from "../Elements/Image";
 import ContactWithUs from "../Elements/ContactWithUs";
 import Logo from "../Elements/Logo";
+import ContentPartial from "../Partials/ContentPartial";
 
 const ContentLayout = () => {
   return (
     <>
-      <Grid container>
-        <Grid item xs={6}>
-          <ContactWithUs />
-        </Grid>
+      <Grid container direction="row">
         <Grid item xs={6}>
           <Logo />
         </Grid>
+        <Grid item xs={6}>
+          <ContactWithUs />
+        </Grid>
       </Grid>
 
-      <Grid container>
-        <Grid item xs={6}>
+      <Grid
+        container
+        direction="row-reverse"
+        justify="space-evenly"
+        alignItems="center"
+      >
+        <Grid item xs={12} sm={5}>
           <Image img="/images/Onboarding-bro.svg" />
         </Grid>
-        <Grid item xs={6}></Grid>
+        <Grid item xs={12} sm={6}>
+          <ContentPartial />
+        </Grid>
       </Grid>
     </>
   );
